67cf6470c7c9823e99b97ecae064b8bdae1a6a3b
radiochild577 <31926209+radiochild577@users.noreply.github.com>
2022-03-28 18:16:11 -0500

    Update youtube_ios_app.js

--- youtube_ios_app.js
+++ youtube_ios_app.js
@@ -1,4 +1,4 @@
 var url = window.location.href;
 const str = url;
-const newStr = str.replace('https://www.youtube.com/watch?v=', 'youtube://');
+const newStr = str.replace('https://m.youtube.com/watch?v=', 'youtube://');
 window.location.href = console.log(newStr);
