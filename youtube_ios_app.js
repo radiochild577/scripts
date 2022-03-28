06d6afcd85a333d65d2690dd70858f4fda07c13a
radiochild577 <31926209+radiochild577@users.noreply.github.com>
2022-03-28 14:35:14 -0500

    Update youtube_ios_app.js

--- youtube_ios_app.js
+++ youtube_ios_app.js
@@ -1,5 +1,1 @@
-function youtube_parser(url){
-    var regExp = /^https?\:\/\/(?:www\.youtube(?:\-nocookie)?\.com\/|m\.youtube\.com\/|youtube\.com\/)?(?:ytscreeningroom\?vi?=|youtu\.be\/|vi?\/|user\/.+\/u\/\w{1,2}\/|embed\/|watch\?(?:.*\&)?vi?=|\&vi?=|\?(?:.*\&)?vi?=)([^#\&\?\n\/<>"']*)/i;
-    var match = url.match(regExp);
-    return (match && match[1].length==11)? match[1] : false;
-}
+window.location.href = `youtube://${window.location.host}${window.location.pathname}`;
