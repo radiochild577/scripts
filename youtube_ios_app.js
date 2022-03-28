eba5a29213bd6f80f9419c8f49a107ca667d3f16
radiochild577 <31926209+radiochild577@users.noreply.github.com>
2022-03-28 15:21:36 -0500

    Update youtube_ios_app.js

--- youtube_ios_app.js
+++ youtube_ios_app.js
@@ -1,1 +1,3 @@
-window.location.href = `youtube://${window.location.host}${window.location.pathname}`;
+results = url.match('(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})')
+videoId = results[1]
+window.location.href = `youtube://${videoId}`;
